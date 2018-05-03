b00f2c31732862f668df83480051e60e111f247d
jflores7077 <jflores7077@baysidehighschool.org>
2018-05-03 09:29:58 -0400

    1.0b

---
+++ sharkss/main.js
@@ -0,0 +1,23 @@
+/*
+Shark Team
+XF
+5/3/18
+*/
+
+/**functions */
+
+/*Change splash page picture every x seconds*/
+function bgImageChange(){
+
+};
+
+function loaded(){
+
+    bgImageChange();
+};
+
+/**functions */
+
+$(document).ready(function(){
+    loaded();
+});
